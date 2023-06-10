71c7544f0a58233299538140e8277e7242708486
Weronika Koga <weronikakoga@gmail.com>
2023-06-10 16:32:12 +0200

    dodana funkcja safe respond

--- Poker_Game/Poker_Game/Dealer.h
+++ Poker_Game/Poker_Game/Dealer.h
@@ -13,7 +13,8 @@ public:
 	int currentPlayer=1;
 	HumanPlayer& human;
 	ComputerPlayer& computer;
-	
+	int creditsFromHuman;
+	int cresitsFromComputer;
 	
 
 	void PlayTurn() override
@@ -23,28 +24,58 @@ public:
 
 	void TakeCard() override
 	{
-		//0. sprawdza czyja kolej i komu ma dac karte
-		if (currentPlayer = 1)
+		//0. sprawdza czy oboje gracze wylozyli na stol tyle samo
+		while (!CreditCheck())
 		{
-			human.TakeCard();
+			if (currentPlayer = 1)
+			{
+				std::cout << "Do you want to:" << "\n" << "1 - match the current bet" << "\n" << "or" << "\n" << "2 - fold your hand";
+				SafeRespond();
+			}
+			else
+			{
+				//funkcja decydujaca czy dolozyc czy spasowac u komputera
+			}
 		}
-		else
-		{
-			computer.TakeCard();
-		}
-		//1. sprawdza czy oboje gracze wylozyli na stol tyle samo
-		//2. doklada karte
-		//3. wyswietla karte
+		//1. doklada karte
+		hand.push_back(deck.GetCard());
+		//2. wyswietla stol
+		ShowTable();	
 	}
+
+	void ShowTable();
 
 	void SetWinner()
 	{
 		//porownuje ktora reka jest silniejsza
 	}
 
+	bool CreditCheck();
+
 	void Pass()
 	{
 
+	}
+
+	int SafeRespond()
+	{
+		int answear=0;
+		
+		int answer;
+		while (true) {
+			std::cin >> answer;
+			if (!std::cin || (answer != 1 && answer != 2)) {
+				std::cout << "Wrong choice. Enter again: " << "\n";
+				std::cin.clear();
+				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+				continue;
+			}
+			else {
+				break;
+			}
+		}
+
+		return answear;
 	}
 
 };
